4e538b29962e82ba097be2d1ad60fdc4be36f83b
marcello luatti <marcello.luatti@e-globe-solutions.com>
2020-09-23 18:09:29 +0200

    🚀 milestone: yup validation schema through resolver + some refactor

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,12 +1,13 @@
 import React, { useEffect, useMemo } from 'react'
 import { Form, Button } from 'antd'
+import { yupResolver } from '@hookform/resolvers'
 import { useForm, useFieldArray, Controller, FormProvider, useFormContext } from 'react-hook-form'
 
 import * as I from './Inputs'
 import * as S from './styles'
 
 const InputSwitch = (props) => {
-  const { name, type } = props
+  const { name, type, CustomRender } = props
   const { register, unregister } = useFormContext()
 
   useEffect(() => {
@@ -45,7 +46,6 @@ const InputSwitch = (props) => {
   case 'repeater':
     return <Repeater {...props} />
   case 'custom':
-    const CustomRender = props.CustomRender
     return <CustomRender {...props} />
   case 'autocomplete':
     return <I.Autocomplete {...props} />
@@ -54,47 +54,28 @@ const InputSwitch = (props) => {
   }
 }
 
-const FormGroup = ({ initialValues, watchedValues, watchedRepeaterValues, name, defaultValue = '', condition, ...props }) => {
-  const { control } = useFormContext()
-
-  const render = useMemo(() => (!condition || condition(watchedValues, watchedRepeaterValues)), [condition, watchedValues, watchedRepeaterValues])
-
-  if (!render) return null
-
-  return (
-    <Controller
-      as={<InputSwitch />}
-      {...props}
-      initialValues={initialValues?.[name]}
-      defaultValue={defaultValue}
-      control={control}
-      name={name}
-    />
-  )
-}
-
 const Repeater = ({ name, items, initialValues, repeatButtonLabel, ...props }) => {
-  const { watch, watchedValues } = useFormContext()
-  const { fields, append } = useFieldArray({ name })
+  const { watch, watchedValues, errors, control } = useFormContext()
+  const { fields, append } = useFieldArray({ name, control })
+
 
   return (
     <>
       {
         fields.map((field, index) => {
-          const watchedRepeaterValues = watch(`${name}[${index}]`)
+          const watchedRepeaterValues = watch(`${name}.${index}`)
           return (
             <S.Repeater>
               {
-                items.map((item) => (
+                items.map((item) => (
                   <FormGroup
-                    key={`${name}[${index}].${item.name}`}
-                    id={`${name}[${index}].${item.name}`}
                     {...props}
                     {...item}
-                    name={`${name}[${index}].${item.name}`}
+                    key={`${name}.${index}.${item.name}`}  
+                    error={errors?.[name]?.[index]?.[item.name]}
+                    name={`${name}.${index}.${item.name}`}
                     watchedValues={watchedValues}
                     watchedRepeaterValues={watchedRepeaterValues}
-                    initialValues={initialValues}
                   />
                 ))
               }
@@ -111,14 +92,36 @@ const Repeater = ({ name, items, initialValues, repeatButtonLabel, ...props }) =
   )
 }
 
-export default ({ onSubmit, inputs, initialValues = {}, formLayout = {}, submitLabel = 'Save' }) => {
-  const methods = useForm({ defaultValues: initialValues, mode: 'onBlur' })
-  const { handleSubmit, watch } = methods
+const FormGroup = ({ label, watchedValues, watchedRepeaterValues, name, condition, error, defaultValue, ...props }) => {
+  const { control } = useFormContext()
+
+  const render = useMemo(() => (!condition || condition(watchedValues, watchedRepeaterValues)), [condition, watchedValues, watchedRepeaterValues])
+
+  if (!render) return null
+
+  return (
+    <>
+      <Controller
+        as={<InputSwitch />}
+        {...props}
+        {...(watchedValues?.[name] ? {} : { defaultValue })}
+        control={control}
+        name={name}
+      />
+      {error && error.message}
+    </>
+  )
+}
+
+export default ({ onSubmit, inputs, validationSchema = {}, initialValues = {}, formLayout = {}, submitLabel = 'Save' }) => {
+  const methods = useForm({ defaultValues: initialValues, mode: 'onBlur', resolver: yupResolver(validationSchema) })
+  const { handleSubmit, watch, errors } = methods
   const watchedValues = watch()
+
   return (
     <FormProvider {...methods}>
       <Form onFinish={handleSubmit(onSubmit)} {...formLayout}>
-        { inputs.map((input) => <FormGroup key={input.name} {...input} watchedValues={watchedValues} initialValues={initialValues} />)}
+        { inputs.map((input) => <FormGroup key={input.name} {...input} watchedValues={watchedValues} error={errors[input.name]} />)}
         <S.SubmitButtonWrapper>
           <Button htmlType="submit" type="submit">
             {submitLabel}
--- src/Form/stories.js
+++ src/Form/stories.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import * as yup from 'yup'
 import Form from './index'
 
 export default {
@@ -7,27 +8,45 @@ export default {
 }
 
 const inputs = [
-  { label:"field 1", name: "field1", type:"text", marci: true},
-  { label:"field 2", defaultValue: "maeas", name: "field2", type:"text", condition: (e) => e.field1 !== "marci" },
-  { label:"field 3", name: "field3", type:"repeater", items: [
-    { label:"field 1", name: "field", type:"text", marci: true},
-    { label:"field 2", name: "field2", type:"text", condition: (e,c) => c?.field !== "marci" },
-    { label:"field 3", name: "field3", type:"repeater", items: [
-      { label:"field 1", defaultValue: "maeas", name: "field", type:"text", marci: true},
-      { label:"field 2", name: "field2", type:"text", condition: (e,c) => c?.field !== "marci" },
-      
+  { label: 'field 1', name: 'field1', type: 'text' },
+  { label: 'field 2', name: 'field2', defaultValue: 'default value', type: 'text', condition: (e) => console.log(e) || e.field1 !== 'marci' },
+  { label: 'field 3', name: 'field3', type: 'repeater', items: [
+    { label: 'field 4', name: 'field4', type: 'text' },
+    { label: 'field 5', name: 'field5', type: 'text', condition: (e, c) => c?.field4 !== 'marci' },
+    { label: 'field 6', name: 'field6', type: 'repeater', items: [
+      { label: 'field 7', name: 'field7', type: 'text'},
+      { label: 'field 8', name: 'field8', type: 'text', condition: (e, c) => c?.field7 !== 'marci' },
     ]}
   ] },
 ]
 
 const initialValues = {
-  field1: "first",
-  field2: "second",
-  field3 : [{
-    field: "inner repeater one"
-  }]
+  field1: 'lorem',
+  field2: 'lorem init',
+  field3: [
+    {
+      field4: 'ipsum',
+    },
+    {
+      field4: 'dolor',
+      field5: 'sit',
+    },
+  ],
 }
 
-const Template = (args) => <Form inputs={inputs} initialValues={initialValues} onSubmit={e => console.log({e})} />
+const validationSchema = yup.object().shape({
+  field1: yup.string().required(),
+  field2: yup.string().required(),
+  field3: yup.array()
+    .of(
+      yup.object().shape({
+        field4: yup.string().required(),
+        field5: yup.string().required(),
+      }),
+    )
+    .required('Required'),
+})
+
+const Template = () => <Form inputs={inputs} validationSchema={validationSchema} initialValues={initialValues} onSubmit={(e) => console.log({e})} />
 
 export const Default = Template.bind({})
