eb0bbfb6f5fc062e39dedea454529c874949ee9f
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-03-01 22:16:11 +0100

    💅🏻 Form: some styles

--- src/Form/styles.js
+++ src/Form/styles.js
@@ -5,7 +5,6 @@ export const Form = styled.div`
   .ant-legacy-form-item-children {
     display: block;
   }
-
   .ant-form-item,
   .ant-legacy-form-item {
     width: 100%;
@@ -34,12 +33,10 @@ export const FormGroup = styled.div`
     
     margin-bottom: 2rem;
   `};
-
   .ant-form-item{
     margin-bottom: .5rem;
     width: 100%;
   }
-
   .ant-collapse {
     margin-top: .25rem;
   }
@@ -48,19 +45,18 @@ export const FormGroup = styled.div`
     padding-top: .25rem !important;
     padding-bottom: .25rem !important;
   }
-
 `
 
 export const Repeater = styled.div`
-  border: 1px solid #d9d9d9;
+  border: 1px dashed #d9d9d9;
   padding: 1rem;
+  padding-top: ${(props) => (props.layout === 'horizontal' ? '2rem' : '1.5rem')};
   margin-bottom: 0.5rem;
   position: relative;
   transition: .2s all;
   position: relative;
-
-  &:hover {
-    border: 1px solid #40a9ff;
+  &:hover{
+    box-shadow: 0 14px 26px rgba(0, 0, 0, 0.04);
   }
 `
 
@@ -77,11 +73,25 @@ export const IconWrapper = styled.div`
     padding: .25rem;
     transition: .2s all ease-in-out;
     border-radius: 2px;
-    color: ${(props) => (props.danger ? 'red' : 'inherit')};
-
-    &:hover{
-      background: ${(props) => (props.danger ? 'red' : '#40a9ff')};
+    
+    ${(props) => props.active && `
       color: white;
+      background-color: #40a9ff;
+    `};
+    ${(props) => props.danger && `
+      color: red;
+    `};
+    &:hover{
+      color: #40a9ff;
+      ${(props) => props.active && `
+        color: white;
+      `};
+
+      ${(props) => props.danger && `
+        background: red;
+        color: white;
+      `};
+
     }
   }
 `
@@ -94,6 +104,10 @@ export const SubmitButtonWrapper = styled.div`
   margin-top: 1rem;
 `
 
+export const FormGroupWrapper = styled.div`
+  ${(props) => !props?.visible && 'display: none'};
+`
+
 export const InputGroup = styled.div`
   margin-bottom: 1.5rem;
   padding: .25rem;
@@ -102,10 +116,13 @@ export const InputGroup = styled.div`
   ${(props) => props.hasError && `
     box-shadow: 0 0 0 2px rgb(255 77 79 / 20%);
   `}
-
   ${(props) => props.layout === 'horizontal' && `
     display: flex;
   `}
+`
+export const RenderPreviewHandlers = styled.div`
+  border-right: 1px solid grey;
+  padding-right: .5rem;
 `
 
 export const InputLabel = styled.label`
@@ -120,7 +137,6 @@ export const InputLabel = styled.label`
 `
 export const InputWrapper = styled.div`
   ${(props) => props.layout === 'horizontal' && `
-
     flex-basis: 100%;
     width: 100%;
   `}
@@ -130,7 +146,6 @@ export const InputError = styled.div`
   color: red;
   font-size: .75rem;
   margin-top: .25rem;
-
   svg {
     background: red;
     border-radius: 50%;
