ba16bd90deb6d471d69498b431e6cfe5f67f0f14
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-03-01 22:13:31 +0100

    🔧 Datepicker: simplified in order to remove moment

--- src/Form/Inputs/Datepicker.js
+++ src/Form/Inputs/Datepicker.js
@@ -1,15 +1,6 @@
 import React from 'react'
-import { DatePicker } from 'antd'
-import moment from 'moment'
+import DatePicker from 'antd/lib/date-picker'
 
-export default (props) => {
-  const { value, style, defaultValue } = props
-  return (
-    <DatePicker
-      {...props}
-      value={value && moment(value)}
-      defaultValue={defaultValue && moment(defaultValue)}
-      style={style || { width: '100%' }}
-    />
-  )
-}
+export default (props) => (
+  <DatePicker {...props} />
+)
