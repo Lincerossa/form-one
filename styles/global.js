9b9dfcc8d22cadd5995b3daa3097418cfd3902f7
marcello luatti <marcello.luatti@e-globe-solutions.com>
2021-03-01 22:14:26 +0100

    🚀 added global styles for demo purpose (see .storybook folder)

---
+++ styles/global.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+import 'antd/dist/antd.css'
+import '@ant-design/compatible/assets/index.css'
+
+export default createGlobalStyle`
+  *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+  }
+
+`
---
+++ styles/theme.js
@@ -0,0 +1,40 @@
+export default {
+  colors: {
+    blue: {
+      main: '#1890ff',
+      active: '#40a9ff',
+      disabled: '#1890ff',
+      light: '#e6f7ff',
+    },
+    green: {
+      main: '#88bd4f',
+      active: '#6e9e3b',
+      disabled: '#6e9e3b',
+    },
+    violet: {
+      main: '#f0f',
+      active: '#ff4bff',
+      disabled: '#ff4bff',
+    },
+    red: {
+      main: '#ff0000',
+      active: '#ff0000',
+      disabled: '#ff0000',
+    },
+    material: {
+      main: '#6200ee',
+    },
+    grey: {
+      main: "#e8e8e8",
+      dark: '#343434',
+      light: '#d8d8d8',
+      lightGrey: '#bdc9cc',
+    },
+    gray: {
+      main: "#e8e8e8",
+      dark: '#343434',
+      light: '#d8d8d8',
+      lightGrey: '#bdc9cc',
+    },
+  },
+}
